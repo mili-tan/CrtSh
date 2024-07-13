badf87a1b62d623f13dd3143da5fb7649055623c
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2024-07-13 21:15:19 +0800

    添加项目文件。

---
+++ CrtSh/CrtshSharp.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CrtSh
+{
+    public class CrtshSharp
+    {
+        public static async Task<List<CertificateInformation>> Search(string query)
+        {
+            var url = $"https://crt.sh/?q={query}&output=json";
+            var response = await new HttpClient().GetStringAsync(url);
+            return JsonSerializer.Deserialize<List<CertificateInformation>>(response) ?? [];
+        }
+
+        public class CertificateInformation
+        {
+            [JsonPropertyName("issuer_ca_id")]
+            public int? IssuerCaId { get; set; }
+
+            [JsonPropertyName("issuer_name")]
+            public string? IssuerName { get; set; }
+
+            [JsonPropertyName("common_name")]
+            public string? CommonName { get; set; }
+
+            [JsonPropertyName("name_value")]
+            public string? NameValue { get; set; }
+
+            [JsonPropertyName("id")]
+            public long? Id { get; set; }
+
+            [JsonPropertyName("entry_timestamp")]
+            public DateTime? EntryTimestamp { get; set; }
+
+            [JsonPropertyName("not_before")]
+            public DateTime? NotBefore { get; set; }
+
+            [JsonPropertyName("not_after")]
+            public DateTime? NotAfter { get; set; }
+
+            [JsonPropertyName("serial_number")]
+            public string? SerialNumber { get; set; }
+
+            [JsonPropertyName("result_count")]
+            public int? ResultCount { get; set; }
+        }
+    }
+}
---
+++ CrtSh/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CrtSh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = CrtshSharp.Search("example.com").Result;
+            foreach (var cert in result)
+            {
+                Console.WriteLine(cert.IssuerName);
+                Console.WriteLine(cert.NotAfter);
+            }
+        }
+    }
+}
