ec7d17a65cfbaa20f2035a016fc577a90c34dc1e
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2024-07-15 19:21:02 +0800

    reminderCerts

--- CrtSh/Program.cs
+++ CrtSh/Program.cs
@@ -2,13 +2,22 @@
 {
     internal class Program
     {
+        public static int RemainingReminderDays = 15;
+        public static int StopReminderDays = 15;
+
+
         static void Main(string[] args)
         {
+            var reminderCerts = new List<CrtshSharp.CertificateInformation>();
             var result = CrtshSharp.Search("example.com").Result;
             foreach (var cert in result)
             {
-                Console.WriteLine(cert.IssuerName);
-                Console.WriteLine(cert.NotAfter);
+                if (cert.NotAfter == null) continue;
+                var days = (cert.NotAfter.Value - DateTime.Now).Days;
+                if (days < RemainingReminderDays && days + StopReminderDays > 0)
+                {
+                    reminderCerts.Add(cert);
+                }
             }
         }
     }
